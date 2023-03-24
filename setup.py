b34bb850d1d5af066ecd3622b275518b4ec9963e
Matteo Magnini <matteo.magnini@studio.unibo.it>
2023-03-24 17:10:07 +0100

    feat: black compliant

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ import distutils.cmd
 
 here = pathlib.Path(__file__).parent.resolve()
 
-version_file = here / 'VERSION'
+version_file = here / "VERSION"
 
 # Get the long description from the README file
-long_description = (here / 'README.md').read_text(encoding='utf-8')
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 
 def format_git_describe_version(version):
-    if '-' in version:
-        splitted = version.split('-')
+    if "-" in version:
+        splitted = version.split("-")
         tag = splitted[0]
         index = f"dev{splitted[1]}"
         return f"{tag}.{index}"
@@ -25,17 +25,19 @@ def format_git_describe_version(version):
 
 def get_version_from_git():
     try:
-        process = subprocess.run(["git", "describe"], cwd=str(here), check=True, capture_output=True)
-        version = process.stdout.decode('utf-8').strip()
+        process = subprocess.run(
+            ["git", "describe"], cwd=str(here), check=True, capture_output=True
+        )
+        version = process.stdout.decode("utf-8").strip()
         version = format_git_describe_version(version)
-        with version_file.open('w') as f:
+        with version_file.open("w") as f:
             f.write(version)
         return version
     except subprocess.CalledProcessError:
         if version_file.exists():
             return version_file.read_text().strip()
         else:
-            return '0.1.0'
+            return "0.1.0"
 
 
 version = get_version_from_git()
@@ -46,7 +48,7 @@ print(f"Detected version {version} from git describe")
 class GetVersionCommand(distutils.cmd.Command):
     """A custom command to get the current project version inferred from git describe."""
 
-    description = 'gets the project version from git describe'
+    description = "gets the project version from git describe"
     user_options = []
 
     def initialize_options(self):
@@ -60,44 +62,44 @@ class GetVersionCommand(distutils.cmd.Command):
 
 
 setup(
-    name='demo-psyki-python',  # Required
+    name="demo-psyki-python",  # Required
     version=version,
-    description='Tutorial of PSyKI technology',
-    license='Apache 2.0 License',
+    description="Tutorial of PSyKI technology",
+    license="Apache 2.0 License",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/psykei/demo-psyki-python',
-    author='Matteo Magnini',
-    author_email='matteo.magnini@unibo.it',
+    long_description_content_type="text/markdown",
+    url="https://github.com/psykei/demo-psyki-python",
+    author="Matteo Magnini",
+    author_email="matteo.magnini@unibo.it",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Prolog'
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Prolog",
     ],
-    keywords='symbolic knowledge injection, ski, symbolic ai',  # Optional
+    keywords="symbolic knowledge injection, ski, symbolic ai",  # Optional
     # package_dir={'': 'src'},  # Optional
     packages=find_packages(),  # Required
     include_package_data=True,
-    python_requires='>=3.9.0, <3.10',
+    python_requires=">=3.9.0, <3.10",
     install_requires=[
-        'jupyter>=1.0.0',
-        'psyki>=0.3.9',
+        "jupyter>=1.0.0",
+        "psyki>=0.3.9",
     ],  # Optional
     zip_safe=False,
     platforms="Independant",
     project_urls={  # Optional
-        'Bug Reports': 'https://github.com/psykei/demo-psyki-python/issues',
+        "Bug Reports": "https://github.com/psykei/demo-psyki-python/issues",
         # 'Funding': 'https://donate.pypi.org',
         # 'Say Thanks!': 'http://saythanks.io/to/example',
-        'Source': 'https://github.com/psykei/demo-psyki-python',
+        "Source": "https://github.com/psykei/demo-psyki-python",
     },
     cmdclass={
-        'get_project_version': GetVersionCommand,
+        "get_project_version": GetVersionCommand,
     },
 )
