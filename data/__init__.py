fdf4ebf23b851c34cb3d0c5735c52bfb9cf97808
Matteo Magnini <matteo.magnini@studio.unibo.it>
2023-03-24 11:46:11 +0100

    feat: continuous integration, check notebooks, release and dockerify

---
+++ data/__init__.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from knowledge import UCI_URL
+
+PATH = Path(__file__).parents[0]
+
+
+class CensusIncome(object):
+    name: str = 'census-income'
+    knowledge_file_name: str = 'census-income.pl'
+    data_file_name: str = 'census-income.csv'
+    data_test_file_name: str = 'census-income-test.csv'
+    data_url: str = UCI_URL + "adult/adult.data"
+    data_test_url: str = UCI_URL + "adult/adult.test"
+    features: list[str] = ["Age", "WorkClass", "Fnlwgt", "Education", "EducationNumeric", "MaritalStatus",
+                           "Occupation", "Relationship", "Ethnicity", "Sex", "CapitalGain", "CapitalLoss",
+                           "HoursPerWeek", "NativeCountry"]
+    target: list[str] = ["income"]
+    class_mapping: dict[str, int] = {'0.0': 0, '1.0': 1}
+    integer_features: list[str] = ["Age", "CapitalGain", "CapitalLoss", "HoursPerWeek"]
+    ordinal_features: list[str] = ["EducationNumeric"]
+    binary_features: list[str] = ["Sex"]
+    nominal_features: list[str] = ["WorkClass", "MaritalStatus", "Occupation", "Relationship", "Ethnicity", "NativeCountry"]
+    droppable_features: list[str] = ["Fnlwgt", "Education"]
--- knowledge/__init__.py
+++ knowledge/__init__.py
@@ -2,20 +2,3 @@ from pathlib import Path
 
 PATH = Path(__file__).parents[0]
 UCI_URL: str = "https://archive.ics.uci.edu/ml/machine-learning-databases/"
-
-
-class CensusIncome(object):
-    name: str = 'census-income'
-    knowledge_file_name: str = 'census-income.pl'
-    data_url: str = UCI_URL + "adult/adult.data"
-    data_test_url: str = UCI_URL + "adult/adult.test"
-    features: list[str] = ["Age", "WorkClass", "Fnlwgt", "Education", "EducationNumeric", "MaritalStatus",
-                           "Occupation", "Relationship", "Ethnicity", "Sex", "CapitalGain", "CapitalLoss",
-                           "HoursPerWeek", "NativeCountry"]
-    target: list[str] = ["income"]
-    class_mapping: dict[str, int] = {'0.0': 0, '1.0': 1}
-    integer_features: list[str] = ["Age", "CapitalGain", "CapitalLoss", "HoursPerWeek"]
-    ordinal_features: list[str] = ["EducationNumeric"]
-    binary_features: list[str] = ["Sex"]
-    nominal_features: list[str] = ["WorkClass", "MaritalStatus", "Occupation", "Relationship", "Ethnicity", "NativeCountry"]
-    droppable_features: list[str] = ["Fnlwgt", "Education"]
